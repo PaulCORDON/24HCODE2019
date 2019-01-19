0beb49dbe9ff3772f5a5c5770eda1d0726e9183d
Jaulin <alexandrejaulin@orange.fr>
2019-01-19 22:51:11 +0100

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/com/ensim/a24h/MainActivity.java
+++ app/src/main/java/com/ensim/a24h/MainActivity.java
@@ -24,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     private Button boutonVert;
     private Button boutonBleu;
     private Button boutonWipe;
+    private Button boutonRainbow;
+    private Button boutonSerpent;
     private CheckBox boule1,boule2,boule3,boule4,boule5,boule6,boule7,boule8,boule9;
 
 
@@ -56,6 +58,8 @@ public class MainActivity extends AppCompatActivity {
         boutonVert = (Button)findViewById(R.id.boutonVert);
         boutonBleu = (Button)findViewById(R.id.boutonBleu);
         boutonWipe = (Button)findViewById(R.id.boutonWipe);
+        boutonRainbow = (Button)findViewById(R.id.boutonRainbow);
+        boutonSerpent = (Button)findViewById(R.id.boutonSerpent);
         boule1 = (CheckBox)findViewById(R.id.checkBox);
         boule2 = (CheckBox)findViewById(R.id.checkBox10);
         boule3 = (CheckBox)findViewById(R.id.checkBox11);
@@ -72,34 +76,200 @@ public class MainActivity extends AppCompatActivity {
 
         boutonRouge.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                mqttService.set_pixel("Laumio_0FBFBF",9,255,0,0);
+                for (String boule : listeBouleCheckees) {
+                    mqttService.set_pixel(boule,9,255,0,0);
+                }
+
             }
         });
         boutonVert.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                mqttService.set_pixel("Laumio_107DA8",9,0,255,0);
+                for (String boule : listeBouleCheckees) {
+                    mqttService.set_pixel(boule,9,0,255,0);
+                }
+
             }
         });
         boutonBleu.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                mqttService.set_pixel("Laumio_107DA8",9,0,0,255);
-            }
-        });
-        boutonLed.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+                for (String boule : listeBouleCheckees) {
+                    mqttService.set_pixel(boule,9,0,0,255);
+                }
 
             }
         });
+
         boutonWipe.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-               mqttService.color_wipe("Laumio_88813D",255,0,0,3000);
+                for (String boule : listeBouleCheckees) {
+                    mqttService.color_wipe(boule,255,0,0,3000);
+                }
+
             }
         });
         boutonRainbow.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                mqttService.animate_rainbow("Laumio_CD0522");
+                for (String boule : listeBouleCheckees) {
+                    mqttService.animate_rainbow(boule);
+                }
+
             }
         });
+        boutonSerpent.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                for (String boule : listeBouleCheckees) {
+                    mqttService.set_pixel(boule,0,255,0,0);
+                    mqttService.set_pixel(boule,1,255,23,0);
+                    mqttService.set_pixel(boule,2,255,46,0);
+                    mqttService.set_pixel(boule,9,255,69,0);
+                    mqttService.set_pixel(boule,3,255,92,0);
+                    mqttService.set_pixel(boule,4,255,115,0);
+                    mqttService.set_pixel(boule,5,255,138,0);
+                    mqttService.set_pixel(boule,6,255,161,0);
+                    mqttService.set_pixel(boule,7,255,184,0);
+                    mqttService.set_pixel(boule,8,255,207,0);
+                    mqttService.set_pixel(boule,9,255,230,0);
+                    mqttService.set_pixel(boule,10,255,240,0);
+                    mqttService.set_pixel(boule,11,255,255,0);
+                    mqttService.set_pixel(boule,12,255,255,0);
+
+                    mqttService.set_pixel(boule,0,0,0,0);
+                    mqttService.set_pixel(boule,1,0,0,0);
+                    mqttService.set_pixel(boule,2,0,0,0);
+                    mqttService.set_pixel(boule,9,0,0,0);
+                    mqttService.set_pixel(boule,3,0,0,0);
+                    mqttService.set_pixel(boule,4,0,0,0);
+                    mqttService.set_pixel(boule,5,0,0,0);
+                    mqttService.set_pixel(boule,6,0,0,0);
+                    mqttService.set_pixel(boule,7,0,0,0);
+                    mqttService.set_pixel(boule,8,0,0,0);
+                    mqttService.set_pixel(boule,9,0,0,0);
+                    mqttService.set_pixel(boule,10,0,0,0);
+                    mqttService.set_pixel(boule,11,0,0,0);
+                    mqttService.set_pixel(boule,12,0,0,0);
+                }
+
+            }
+        });
+
+        boule1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(boule1.isChecked()){
+                    listeBouleCheckees.add("Laumio_1D9486");
+                }
+                if(!boule1.isChecked()){
+                    listeBouleCheckees.remove("Laumio_1D9486");
+                    mqttService.fill(0,0,0);
+                }
+
+            }
+        });
+        boule2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(boule2.isChecked()){
+                    listeBouleCheckees.add("Laumio_104A13");
+                }
+                if(!boule2.isChecked()){
+                    listeBouleCheckees.remove("Laumio_104A13");
+                    mqttService.fill(0,0,0);
+                }
+
+            }
+        });
+        boule3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(boule3.isChecked()){
+                    listeBouleCheckees.add("Laumio_104F03");
+                }
+                if(!boule3.isChecked()){
+                    listeBouleCheckees.remove("Laumio_104F03");
+                    mqttService.fill(0,0,0);
+                }
+
+            }
+        });
+        boule4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(boule4.isChecked()){
+                    listeBouleCheckees.add("Laumio_10508F");
+                }
+                if(!boule4.isChecked()){
+                    listeBouleCheckees.remove("Laumio_10508F");
+                    mqttService.fill(0,0,0);
+                }
+
+            }
+        });
+        boule5.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(boule5.isChecked()){
+                    listeBouleCheckees.add("Laumio_0FBFBF");
+                }
+                if(!boule5.isChecked()){
+                    listeBouleCheckees.remove("Laumio_0FBFBF");
+                    mqttService.fill(0,0,0);
+                }
+
+            }
+        });
+        boule6.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(boule6.isChecked()){
+                    listeBouleCheckees.add("Laumio_10805F");
+                }
+                if(!boule6.isChecked()){
+                    listeBouleCheckees.remove("Laumio_10805F");
+                    mqttService.fill(0,0,0);
+                }
+
+            }
+        });
+        boule7.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(boule7.isChecked()){
+                    listeBouleCheckees.add("Laumio_CD0522");
+                }
+                if(!boule7.isChecked()){
+                    listeBouleCheckees.remove("Laumio_CD0522");
+                    mqttService.fill(0,0,0);
+                }
+
+            }
+        });
+        boule8.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(boule8.isChecked()){
+                    listeBouleCheckees.add("Laumio_0FC168");
+                }
+                if(!boule8.isChecked()){
+                    listeBouleCheckees.remove("Laumio_0FC168");
+                    mqttService.fill(0,0,0);
+                }
+
+            }
+        });
+        boule9.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(boule9.isChecked()){
+                    listeBouleCheckees.add("Laumio_D454DB");
+                }
+                if(!boule9.isChecked()){
+                    listeBouleCheckees.remove("Laumio_D454DB");
+                    mqttService.fill(0,0,0);
+                }
+
+            }
+        });
+
 
 
 

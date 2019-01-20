a96949692172d696e2c7d23cb2f6f25dfc0fa9ee
Jaulin <alexandrejaulin@orange.fr>
2019-01-20 02:54:13 +0100

    nouveau pattern PatternNeige

---
+++ app/src/main/java/com/ensim/a24h/Patern/PatternNeige.java
@@ -0,0 +1,82 @@
+package com.ensim.a24h.Patern;
+
+import com.ensim.a24h.MqttServices;
+
+import java.util.ArrayList;
+
+public class PatternNeige implements IPattern{
+    ArrayList<String> listeBoules = new ArrayList<String>();
+
+    PatternNeige(ArrayList<String> s){
+        listeBoules = s;
+    }
+
+    @Override
+    public void execute() {
+        MqttServices mqttService = new MqttServices();
+        for (String boule : listeBoules) {
+            mqttService.set_pixel(boule, 9, 0, 97, 255);
+        }
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (String boule : listeBoules) {
+            mqttService.set_ring(boule, 2, 89, 152, 255);
+        }
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        for (String boule : listeBoules) {
+            mqttService.set_ring(boule, 1, 178, 207, 255);
+        }
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        for (String boule : listeBoules) {
+            mqttService.set_ring(boule, 0, 255, 255, 255);
+        }
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (String boule : listeBoules) {
+            mqttService.set_pixel(boule, 9, 0, 0, 0);
+        }
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (String boule : listeBoules) {
+            mqttService.set_ring(boule, 2, 0, 0, 0);
+        }
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        for (String boule : listeBoules) {
+            mqttService.set_ring(boule, 1, 0, 0, 0);
+        }
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        for (String boule : listeBoules) {
+            mqttService.set_ring(boule, 0, 0, 0, 0);
+        }
+
+    }
+}
